82c564a5a9a89b671b75495f4be7b31df1db717a
sha <urabummer4@hotmail.com>
2019-02-04 01:14:25 +0800

    nav dropdown, static pages and controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
 
   resources :insights, only: [:index]
 
-  root 'dates#index'
+  get 'statics/exhibit'
+  get 'statics/dev'
+
+  root 'insights#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
